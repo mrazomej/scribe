30d619757e8db1e3bffc54e64ca70b4549d2707a
JAX Toolbox <jax@nvidia.com>
2025-02-16 15:35:34 +0000

    exploring 10xGenomics 50:50 dataset

---
+++ code/processing/10xGenomics/50-50_Jurkat-293T_mixture/nbdm_mix_fit.py
@@ -0,0 +1,88 @@
+# %% ---------------------------------------------------------------------------
+
+# Import base libraries
+import os
+import glob
+import gc
+import pickle
+import scanpy as sc
+
+# Import pandas for data manipulation
+import pandas as pd
+# Import scribe
+import scribe
+
+# Import JAX-related libraries
+import jax
+from jax import random
+import jax.numpy as jnp
+
+# ------------------------------------------------------------------------------
+
+# %% ---------------------------------------------------------------------------
+
+print("Defining inference parameters...")
+
+# Define model_type
+model_type = "nbdm_mix"
+
+# Define training parameters
+n_steps = 30_000
+
+# Define number of components in mixture model
+n_components = 2
+
+# Define prior parameters
+prior_params = {
+    "p_prior": (1, 1),
+    "r_prior": (2, 0.075),
+    "mixing_prior": (1, 1),
+}
+
+# %% ---------------------------------------------------------------------------
+
+print("Setting directories...")
+
+# Define data directory
+DATA_DIR = f"{scribe.utils.git_root()}/data/" \
+           f"10xGenomics/50-50_Jurkat-293T_mixture"
+
+# Define output directory
+OUTPUT_DIR = f"{scribe.utils.git_root()}/output/" \
+             f"10xGenomics/50-50_Jurkat-293T_mixture/{model_type}"
+
+# Create output directory if it doesn't exist
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+# %% ---------------------------------------------------------------------------
+
+print("Loading data...")
+
+# Load data
+data = sc.read_h5ad(f"{DATA_DIR}/data.h5ad")
+
+# %% ---------------------------------------------------------------------------
+
+print("Running inference...")
+
+
+# Define file name
+file_name = f"{OUTPUT_DIR}/" \
+    f"scribe_{model_type}_results_" \
+    f"{n_components:02d}components_" \
+    f"{n_steps}steps.pkl"
+
+# Check if the file exists
+if not os.path.exists(file_name):
+    # Run scribe
+    scribe_results = scribe.svi.run_scribe(
+        model_type=model_type,
+        counts=data,
+        n_steps=n_steps,
+        n_components=n_components,
+        prior_params=prior_params,
+    )
+
+    # Save the results, the true values, and the counts
+    with open(file_name, "wb") as f:
+        pickle.dump(scribe_results, f)
