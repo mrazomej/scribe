718a97bc2bf8578749bffdc3143c87a475a64eb4
mrazomej <manuel.razo.m@gmail.com>
2025-06-18 19:37:33 +0000

    More Singer processing script updates

--- code/processing/singer/mcmc_standard_zinb_singer.py
+++ code/processing/singer/mcmc_standard_zinb_singer.py
@@ -44,6 +44,9 @@ import scribe
 # Define model type
 model_type = "zinb"
 
+# Define parameterization type
+parameterization = "standard"
+
 # Define data directory
 DATA_DIR = f"{scribe.utils.git_root()}/data/singer/"
 # Define output directory
@@ -83,14 +86,15 @@ jax.clear_caches()
 
 # Define output file name
 file_name = f"{OUTPUT_DIR}/" \
-        f"mcmc_constrained_{model_type}_results_" \
+        f"mcmc_{parameterization.replace('_', '-')}_" \
+        f"{model_type}_results_" \
         f"{n_cells}cells_" \
         f"{n_genes}genes_" \
         f"{n_mcmc_burnin}burnin_" \
         f"{n_mcmc_samples}samples.pkl"
 
-# Define kernel kwargs
-kernel_kwargs = {
+# Define MCMC kwargs
+mcmc_kwargs = {
     "target_accept_prob": 0.85,
     "max_tree_depth": (10, 10),
     "step_size": jnp.array(1.0, dtype=jnp.float64),
@@ -103,13 +107,15 @@ kernel_kwargs = {
 
 if not os.path.exists(file_name):
     # Run MCMC sampling
-    mcmc_results = scribe.mcmc.run_scribe(
+    mcmc_results = scribe.run_scribe(
+        inference_method="mcmc",
         counts=data,
-        unconstrained_model=False,
         zero_inflated=True,
-        num_warmup=n_mcmc_burnin,
-        num_samples=n_mcmc_samples,
-        kernel_kwargs=kernel_kwargs,
+        parameterization=parameterization,
+        n_warmup=n_mcmc_burnin,
+        n_samples=n_mcmc_samples,
+        mcmc_kwargs=mcmc_kwargs,
+        phi_prior=(3, 2),
     )
     # Save MCMC results
     with open(file_name, "wb") as f:
