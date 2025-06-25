6b91cd955777cc96c6b21d65d6cbe29722737c49
mrazomej <manuel.razo.m@gmail.com>
2025-06-25 22:11:31 +0000

    Add new scripts for SVI fitting and visualization across multiple models
    Introduced new Python scripts for fitting and visualizing the SVI (Stochastic Variational Inference) method for various models including NBDM, NBVCP, ZINB, and ZINBVCP. Each model has dedicated fitting and visualization scripts that handle data loading, inference, and result plotting, enhancing the modularity and clarity of the codebase. This update supports improved analysis workflows and facilitates easier model comparisons.

---
+++ code/processing/sanity_datasets/svi_odds-ratio_nbdm_sanity_fit.py
@@ -0,0 +1,101 @@
+# %% ---------------------------------------------------------------------------
+
+# Import base libraries
+import os
+import glob
+import gc
+import pickle
+
+# Import pandas for data manipulation
+import pandas as pd
+# Import scribe
+import scribe
+
+# Import JAX-related libraries
+import jax
+from jax import random
+import jax.numpy as jnp
+from numpyro.optim import Adam
+import numpyro
+
+# %% ---------------------------------------------------------------------------
+
+# Define model_type
+model_type = "nbdm"
+
+# Define parameterization
+parameterization = "odds_ratio"
+
+# Define training parameters
+n_steps = 50_000
+batch_size_max = 2048
+
+# %% ---------------------------------------------------------------------------
+
+# Define data directory
+DATA_DIR = f"/app/data/sanity"
+
+# Define output directory
+OUTPUT_DIR = f"{scribe.utils.git_root()}/output/sanity/{model_type}"
+
+# Create output directory if it doesn't exist
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+# %% ---------------------------------------------------------------------------
+
+# List all files in the data directory
+files = glob.glob(f"{DATA_DIR}/*counts.txt.gz", recursive=True)
+# %% ---------------------------------------------------------------------------
+
+# Loop over files
+for file in files:
+    print(f"Processing {file}")
+    # Extract dataset name from file name
+    dataset_name = file.split("/")[-1].replace("_counts.txt.gz", "")
+
+    # Define output file name
+    file_name = f"{OUTPUT_DIR}/" \
+                f"svi_{parameterization.replace('_', '-')}_" \
+                f"{model_type.replace('_', '-')}_" \
+                f"{n_steps}steps_" \
+                f"{dataset_name}.pkl"
+
+    # Check if the file exists
+    if os.path.exists(file_name):
+        print(f"Skipping {file} because it already exists")
+        continue
+
+    print(f"Loading data for {dataset_name}...")
+    # Load data
+    df = pd.read_csv(file, sep="\t", index_col=0, compression="gzip")
+
+    # Define batch size based on the number of cells
+    batch_size = batch_size_max if df.shape[1] >= batch_size_max else None
+
+    # Clear JAX caches
+    jax.clear_caches()
+    # Clear memory
+    gc.collect()
+
+    print(f"Running inference for {dataset_name}...")
+
+    # Run SCRIBE
+    scribe_result = scribe.run_scribe(
+        inference_method="svi",
+        counts=jnp.array(df.values),
+        parameterization=parameterization,
+        n_steps=n_steps,
+        batch_size=batch_size,
+        cells_axis=1,
+        seed=42,
+    )
+
+    print(f"Saving results for {dataset_name}...")
+
+    # Save the results
+    with open(file_name, "wb") as f:
+        pickle.dump(scribe_result, f)
+
+    
+
+# %% ---------------------------------------------------------------------------
