a175cdecac6a0b9dfa52d024a60f33c1f4d3f42b
mrazomej <manuel.razo.m@gmail.com>
2025-07-21 15:30:54 +0000

    update script to run all sanity inferences

--- code/processing/sanity_datasets/run_inference.sh
+++ code/processing/sanity_datasets/run_inference.sh
@@ -1,12 +1,12 @@
 echo "Running inference for NBDM..."
-python code/processing/sanity_datasets/nbdm_gamma_sanity_datasets.py
-python code/processing/sanity_datasets/nbdm_lognormal_sanity_datasets.py
+python code/processing/sanity_datasets/svi_odds-ratio_nbdm_sanity_fit.py
+python code/processing/sanity_datasets/svi_odds-ratio_nbdm_sanity_viz.py
 echo "Running inference for ZINB..."
-python code/processing/sanity_datasets/zinb_gamma_sanity_datasets.py
-python code/processing/sanity_datasets/zinb_lognormal_sanity_datasets.py
+python code/processing/sanity_datasets/svi_odds-ratio_zinb_sanity_fit.py
+python code/processing/sanity_datasets/svi_odds-ratio_zinb_sanity_viz.py
 echo "Running inference for NBVCP..."
-python code/processing/sanity_datasets/nbvcp_gamma_sanity_datasets.py
-python code/processing/sanity_datasets/nbvcp_lognormal_sanity_datasets.py
+python code/processing/sanity_datasets/svi_odds-ratio_nbvcp_sanity_fit.py
+python code/processing/sanity_datasets/svi_odds-ratio_nbvcp_sanity_viz.py
 echo "Running inference for ZINBVCP..."
-python code/processing/sanity_datasets/zinbvcp_gamma_sanity_datasets.py
-python code/processing/sanity_datasets/zinbvcp_lognormal_sanity_datasets.py
+python code/processing/sanity_datasets/svi_odds-ratio_zinbvcp_sanity_fit.py
+python code/processing/sanity_datasets/svi_odds-ratio_zinbvcp_sanity_viz.py
