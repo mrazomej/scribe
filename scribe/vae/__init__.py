29246a5869e945882c09d610bef41e44ff8daff0
mrazomej <manuel.razo.m@gmail.com>
2025-07-23 19:25:58 +0000

    Generate functions to build the encoder and the decoder separately when calling the model and guide

--- scribe/vae/__init__.py
+++ scribe/vae/__init__.py
@@ -3,15 +3,23 @@ VAE module for scRNA-seq data analysis.
 """
 
 from .architectures import (
-    VAE,
     VAEConfig,
+    Encoder,
+    Decoder,
+    VAE,
+    create_encoder,
+    create_decoder,
     create_vae,
 )
 from .results import ScribeVAEResults
 
 __all__ = [
-    "VAE",
     "VAEConfig",
+    "Encoder", 
+    "Decoder",
+    "VAE",
+    "create_encoder",
+    "create_decoder",
     "create_vae",
     "ScribeVAEResults",
 ] 
