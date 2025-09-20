2c930a5a6d5bfcfe3afa601d11a462869b69ac91
mrazomej <manuel.razo.m@gmail.com>
2025-09-20 17:28:41 +0000

    Add sphinx-gallery dependency and update documentation configuration
    - Added `sphinx-gallery` as a dependency in `pyproject.toml` and `uv.lock`.
    - Updated Sphinx configuration in `conf.py` to include `sphinx_gallery.gen_gallery` for generating example galleries.
    - Revised `quickoverview.rst` and `quickstart.rst` to enhance clarity and structure, including a unified framework for inference.
    - Introduced example scripts in `auto_examples` and `examples` directories for quickstart guidance.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,27 +6,29 @@
 # Configuration file for the Sphinx documentation builder.
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'SCRIBE'
-copyright = '2025, Manuel Razo'
-author = 'Manuel Razo'
+project = "SCRIBE"
+copyright = "2025, Manuel Razo"
+author = "Manuel Razo"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
-    'myst_parser',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "myst_parser",
+    "sphinx_gallery.gen_gallery",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
@@ -34,21 +36,34 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # Theme
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 # Static path
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Theme options
 html_theme_options = {
-    'navigation_depth': 4,
-    'collapse_navigation': True,
-    'sticky_navigation': True,
+    "navigation_depth": 4,
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+}
+
+# -- Sphinx-gallery configuration --------------------------------------------
+from sphinx_gallery.sorting import FileNameSortKey
+
+sphinx_gallery_conf = {
+    "examples_dirs": "examples",  # path to your example scripts
+    "gallery_dirs": (
+        "auto_examples"
+    ),  # path to where to save gallery generated output
+    "within_subsection_order": FileNameSortKey,
+    "filename_pattern": r"\.py",
+    "run_stale_examples": True,  # Re-run examples that have changed
 }
 
 # -- Extension configuration -------------------------------------------------
 
-autodoc_member_order = 'bysource'
-autodoc_typehints = 'description'
+autodoc_member_order = "bysource"
+autodoc_typehints = "description"
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = False
@@ -64,8 +79,8 @@ napoleon_type_aliases = None
 
 # Intersphinx mapping
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'numpy': ('https://numpy.org/doc/stable/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
-    'jax': ('https://jax.readthedocs.io/en/latest/', None),
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+    "jax": ("https://jax.readthedocs.io/en/latest/", None),
 }
